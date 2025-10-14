b1c91581056573b37ba0c45af8282e564bda1758
Harry Brizuela <Harrycueva262008@gmail.com>
2025-10-13 22:20:58 -0600

    cv

--- Semana_5/Reconocer_Palindromo.py
+++ Semana_5/Reconocer_Palindromo.py
@@ -1,4 +1,4 @@
-reconocer_palindromo = input("Ingrese una palabra para ver si es un palindromo: ").lower()
+reconocer_palindromo = input("Vaya mi labubu ingrese una palabra para ver si es un palindromo: ").lower()
 inverso = reconocer_palindromo[::-1]
 if reconocer_palindromo == inverso:
     print(f"{reconocer_palindromo.upper}Es un palindromo")
